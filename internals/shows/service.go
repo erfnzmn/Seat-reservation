b6db8d0f15f4b33abd88c782dfd81046223e62ef
erfnzmn <fanzm1316@gmail.com>
2025-11-23 19:54:06 +0330

    remove adminKey from service layer to enforce admin check at handler level

--- internals/shows/service.go
+++ internals/shows/service.go
@@ -1,16 +1,16 @@
 package shows
 
 import (
-	"time"
 	"errors"
+	"time"
 )
 
 type Service interface {
 	GetAllShows() ([]Show, error)
 	GetShowByID(id uint) (*Show, error)
-	CreateShow(input CreateShowInput, adminKey string) (*Show, error)
-	UpdateShow(id uint, input UpdateShowInput, adminKey string) (*Show, error)
-	DeleteShow(id uint, adminKey string) error
+	CreateShow(input CreateShowInput) (*Show, error)
+	UpdateShow(id uint, input UpdateShowInput) (*Show, error)
+	DeleteShow(id uint) error
 }
 
 type service struct {
@@ -22,7 +22,6 @@ func NewService(repo Repository) Service {
 }
 
 type CreateShowInput struct {
-	AdminKey  string    `json:"admin_key"`
 	HallID    uint      `json:"hall_id"`
 	MovieName string    `json:"movie_name"`
 	StartTime time.Time `json:"start_time"`
@@ -30,7 +29,6 @@ type CreateShowInput struct {
 }
 
 type UpdateShowInput struct {
-	AdminKey  string    `json:"admin_key"`
 	MovieName string    `json:"movie_name"`
 	StartTime time.Time `json:"start_time"`
 	EndTime   time.Time `json:"end_time"`
@@ -44,11 +42,7 @@ func (s *service) GetShowByID(id uint) (*Show, error) {
 	return s.repo.GetByID(id)
 }
 
-func (s *service) CreateShow(input CreateShowInput, adminKey string) (*Show, error) {
-
-	if input.AdminKey != adminKey {
-        return nil, errors.New("forbidden: invalid admin key")
-    }
+func (s *service) CreateShow(input CreateShowInput) (*Show, error) {
 
 	if input.MovieName == "" {
 		return nil, errors.New("movie name is required")
@@ -72,11 +66,8 @@ func (s *service) CreateShow(input CreateShowInput, adminKey string) (*Show, err
 	return show, nil
 }
 
-func (s *service) UpdateShow(id uint, input UpdateShowInput, adminKey string) (*Show, error) {
+func (s *service) UpdateShow(id uint, input UpdateShowInput) (*Show, error) {
 
-    if input.AdminKey != adminKey {
-        return nil, errors.New("forbidden: invalid admin key")
-    }
 	show, err := s.repo.GetByID(id)
 	if err != nil {
 		return nil, err
@@ -99,10 +90,6 @@ func (s *service) UpdateShow(id uint, input UpdateShowInput, adminKey string) (*
 	return show, nil
 }
 
-func (s *service) DeleteShow(id uint, adminKey string) error {
-
-	if adminKey == "" {
-        return errors.New("forbidden: invalid admin key")
-    }
+func (s *service) DeleteShow(id uint) error {
 	return s.repo.Delete(id)
 }
