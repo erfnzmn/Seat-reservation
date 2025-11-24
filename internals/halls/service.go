f77bcbdb7a9f1cab5dbb5b4de3cce38846960250
erfnzmn <fanzm1316@gmail.com>
2025-11-24 14:13:15 +0330

    Add service layer for Halls and Seats

--- internals/halls/service.go
+++ internals/halls/service.go
@@ -1,1 +1,22 @@
 package halls
+
+type Service interface{
+	GetAllHalls() ([]Hall, error)
+    GetHallByID(id uint) (*Hall, error)
+}
+
+type service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) Service{
+	return &service{repo: repo}
+}
+
+func (s *service) GetAllHalls() ([]Hall, error){
+	return s.repo.GetAll()
+}
+
+func (s *service) GetHallByID(id uint) (*Hall, error) {
+    return s.repo.GetByID(id)
+}
