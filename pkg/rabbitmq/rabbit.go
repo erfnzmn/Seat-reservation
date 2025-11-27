cdcd790e9dff7f8ab2ae0e0afc9db0b4ed275075
erfnzmn <fanzm1316@gmail.com>
2025-11-27 23:39:49 +0330

    simplify RabbitMQ wrapper

--- pkg/rabbitmq/rabbit.go
+++ pkg/rabbitmq/rabbit.go
@@ -1,9 +1,11 @@
 package rabbitmq
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/streadway/amqp"
 )
-
 
 type RabbitMQ struct {
 	Conn    *amqp.Connection
@@ -13,23 +15,85 @@ type RabbitMQ struct {
 func NewRabbitMQ(url string) (*RabbitMQ, error) {
 	conn, err := amqp.Dial(url)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("rabbitmq dial error: %w", err)
 	}
 
 	ch, err := conn.Channel()
 	if err != nil {
 		conn.Close()
-		return nil, err
+		return nil, fmt.Errorf("rabbitmq channel error: %w", err)
 	}
 
-	return &RabbitMQ{Conn: conn, Channel: ch}, nil
+	return &RabbitMQ{
+		Conn:    conn,
+		Channel: ch,
+	}, nil
 }
 
 func (r *RabbitMQ) Close() {
 	if r.Channel != nil {
-		r.Channel.Close()
+		_ = r.Channel.Close()
 	}
 	if r.Conn != nil {
-		r.Conn.Close()
+		_ = r.Conn.Close()
 	}
+}
+
+func (r *RabbitMQ) Publish(queue string, payload any) error {
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("rabbitmq marshal error: %w", err)
+	}
+
+	_, err = r.Channel.QueueDeclare(
+		queue,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return fmt.Errorf("rabbitmq queue declare error: %w", err)
+	}
+
+	return r.Channel.Publish(
+		"",
+		queue,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        body,
+		},
+	)
+}
+
+func (r *RabbitMQ) Consume(queue string) (<-chan amqp.Delivery, error) {
+	_, err := r.Channel.QueueDeclare(
+		queue,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("rabbitmq queue declare error: %w", err)
+	}
+
+	msgs, err := r.Channel.Consume(
+		queue,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("rabbitmq consume error: %w", err)
+	}
+
+	return msgs, nil
 }
